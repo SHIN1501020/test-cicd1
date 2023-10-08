7fb20232cdeda141a49256fe554d11662bc6c2d2
SHIN1501020 <shin8hh@gmail.com>
2023-10-09 00:35:32 +0900

    Update: cicd 파일 수정

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+# 프로젝트 위치 설정
+REPOSITORY=/home/ubuntu/test-cicd1
+
+# 프로젝트 위치로 이동
+cd $REPOSITORY
+
+# 의존성 설치
+echo "> install dependency"
+npm install
+
+# 프로젝트 build
+echo "> build application"
+nest build
+
+# application 재실행
+echo "> reload application"
+pm2 reload test-cicd1
